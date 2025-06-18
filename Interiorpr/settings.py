5210ac6e2cf87b4f47b97dd90025d23d65eef843
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-06-18 23:08:13 +0300

    Project changed

--- Interiorpr/settings.py
+++ Interiorpr/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'intapp',
+    "corsheaders",
     'rest_framework',
     'rest_framework_simplejwt'
 ]
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -152,3 +154,5 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = 'intapp.CustomUser'
+
+CORS_ALLOW_ALL_ORIGINS = True
